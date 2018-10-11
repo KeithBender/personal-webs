a6121a90eca86e55826dc6e21aab11bab2cba8b3
Keith Bender <k.bender30@gmail.com>
2018-10-10 21:16:37 -0500

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,12 @@
-$(window).on('load', function() {
-    $("#loader").fadeOut(2000);
-});
-
 ScrollReveal().reveal('.sr', {
-    reset : true,
+    reset: true,
     duration: 1000
 });
 
-window.onbeforeunload = function () {
-    window.scrollTo(0,0);
-}
-
-$(".side-nav a").click(function(event){
+$(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
-    var aid = $(this).attr("href");
-    if(screen.width > 996){
-        $('html,body').animate({
-            scrollTop: $(aid).offset().top
-        }, 1000,'swing');
-    } else {
-        $('html,body').animate({
-            scrollTop: $(aid).offset().top
-        }, 1000,'swing');
-        toggleNavMenu();
-    }  
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
 });
-
-$(".menu-container").click(function(){
-    toggleNavMenu();
-});
-
-
-function toggleNavMenu(){
-    $(".side-nav").toggleClass("display");
-    $(".top-bar").toggleClass("rotate-45");
-    $(".bottom-bar").toggleClass("rotate-other45");
-}
